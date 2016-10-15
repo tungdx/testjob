51ce2021c91e65672b40c8db69fe0e91e7cbabca
tungdx <tungdx1210@gmail.com>
2016-10-15 21:52:00 +0700

    change limit file size

--- app/src/main/java/vn/john/testjob/Config.java
+++ app/src/main/java/vn/john/testjob/Config.java
@@ -5,7 +5,7 @@ package vn.john.testjob;
  */
 public class Config {
     public static final String FILE_TYPE = ".mp4";
-    public static final int FILE_SIZE_LIMIT = 1 * 1000000;
+    public static final int FILE_SIZE_LIMIT = 20 * 1000000;
     public static final String FIREBASE_STORAGE_REFERENCE = "gs://testjob-5bc57.appspot.com";
 
 
